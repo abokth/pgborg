0bfe5cd5cbeef6b71a7403c4f0d5f282d047cb7c
Alexander Boström <abo@kth.se>
2021-08-02 15:02:55 +0200

    version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pgborg",
-    version="0.0.1",
+    version="0.0.2",
     author="Alexander Boström",
     author_email="abo@kth.se",
     description="Services and utilities for continous archiving and regular full backups of PostgreSQL",
